991e48b66f0dc98d15d4fb08068403d05ba264a5
Kwed <159473699+Kwed1@users.noreply.github.com>
2024-12-30 00:48:36 +0400

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useLayoutEffect, useState } from "react";
-import FirstSection from "./components/FirstSection";
-import FiveSection from "./components/FiveSection";
-import Footer from "./components/Footer";
-import FourSection from "./components/FourSection";
-import Header from "./components/Header";
-import Loader from "./components/Loader";
-import ThreeSection from "./components/ThreeSection";
+import { useLayoutEffect, useState } from "react"
+import FirstSection from "./components/FirstSection"
+import FiveSection from "./components/FiveSection"
+import Footer from "./components/Footer"
+import FourSection from "./components/FourSection"
+import Header from "./components/Header"
+import ThreeSection from "./components/ThreeSection"
 
 export default function App() {
     const [loading, setLoading] = useState<boolean>(true);
